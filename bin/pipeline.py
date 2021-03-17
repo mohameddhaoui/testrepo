7f3159eb865eb07115f02fb524ea520415af659c
mohameddhaoui <49007999+mohameddhaoui@users.noreply.github.com>
2021-03-17 21:42:21 +0100

    add pipeline main

---
+++ bin/pipeline.py
@@ -0,0 +1,21 @@
+import sys
+
+import click
+
+
+@click.group(invoke_without_command=True)
+@click.option(
+    "-s",
+    "--source",
+    required=True,
+    help="Data source name.",
+)
+@click.pass_context
+def cli(    ctx: click.Context,
+
+    source: str
+    ):
+    """
+    Root command.
+    """
+    print(source)
